e319726ee007e4251efedb8d5eb17371bb5bd808
Kamrul Hasan <mdkamrulhasanewu@gmail.com>
2026-09-16 00:58:11 +0600

    feat: add checkbox for same as current address and implement related functionality in address info component

--- frontend/src/app/features/public-registration/components/address-info/address-info.component.ts
+++ frontend/src/app/features/public-registration/components/address-info/address-info.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, DestroyRef, inject, Input, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-address-info',
@@ -7,6 +8,45 @@ import { FormGroup, ReactiveFormsModule } from '@angular/forms';
   imports: [ReactiveFormsModule],
   templateUrl: './address-info.component.html',
 })
-export class AddressInfoComponent {
+export class AddressInfoComponent implements OnInit {
   @Input({ required: true }) form!: FormGroup;
+
+  private readonly destroyRef = inject(DestroyRef);
+
+  sameAsCurrentAddress = new FormControl(false);
+
+  ngOnInit(): void {
+    this.sameAsCurrentAddress.valueChanges
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((isSame) => this.applySameAsCurrentAddress(isSame ?? false));
+
+    this.currentAddressGroup.valueChanges
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => {
+        if (this.sameAsCurrentAddress.value) {
+          this.copyCurrentAddressToPermanent();
+        }
+      });
+  }
+
+  private get currentAddressGroup(): FormGroup {
+    return this.form.get('currentAddress') as FormGroup;
+  }
+
+  private get permanentAddressGroup(): FormGroup {
+    return this.form.get('permanentAddress') as FormGroup;
+  }
+
+  private applySameAsCurrentAddress(isSame: boolean): void {
+    if (isSame) {
+      this.copyCurrentAddressToPermanent();
+      this.permanentAddressGroup.disable({ emitEvent: false });
+    } else {
+      this.permanentAddressGroup.enable({ emitEvent: false });
+    }
+  }
+
+  private copyCurrentAddressToPermanent(): void {
+    this.permanentAddressGroup.patchValue(this.currentAddressGroup.value, { emitEvent: false });
+  }
 }
